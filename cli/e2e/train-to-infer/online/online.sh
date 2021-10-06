50e01470b838662330dab903a64c002128180596
Banibrata De <banibrata@outlook.com>
2021-10-06 11:30:52 -0700

    add comments

--- cli/e2e/train-to-infer/online/online.sh
+++ cli/e2e/train-to-infer/online/online.sh
@@ -3,11 +3,13 @@
 #Comment out and create compute name "cpu-cluster" if it does not exists already
 #az ml compute create -n cpu-cluster --type amlcompute
 
-echo "Creainge the training job "
+sep="######################"
+
+echo "$sep Creating the training job "
 run_id=$(az ml job create -f jobs/scikit-learn/iris/job.yml --query name -o tsv)
 
 
-echo " Checking status of the job with run id $run_id, press ^Ctrl+C to come out "
+echo "$sep Checking status of the job with run id $run_id, press ^Ctrl+C to come out "
 status=$(az ml job show -n $run_id --query status -o tsv)
 
 
@@ -20,12 +22,12 @@ do
 done
 
 
-echo " Download job logs including model, into the current path with  $run_id path name "
+echo "$sep Download job logs including model, into the current path with  $run_id path name "
 az ml job download -n $run_id
 
 #Copy the model into the model path for deployment to pick it up.
 
-echo " Copying model to model folder path "
+echo "$sep Copying model to model folder path "
 
 cp $run_id/model/model.pkl ./model/model.pkl
 
@@ -33,14 +35,14 @@ cp $run_id/model/model.pkl ./model/model.pkl
 export ENDPOINT_NAME=endpt-`echo $RANDOM`
 
 
-echo " Creating endpoint $ENDPOINT_NAME "
+echo "$sep Creating endpoint $ENDPOINT_NAME "
 az ml online-endpoint create --name $ENDPOINT_NAME
 
-echo " Creating deployment "
+echo "$sep Creating deployment "
 az ml online-deployment create --name blue --endpoint $ENDPOINT_NAME -f blue-deployment.yml --all-traffic
 
-echo " Invoking endpoint "
+echo "$sep Invoking endpoint "
 az ml online-endpoint invoke --name $ENDPOINT_NAME --request-file sample-request.json
 
-echo " Deleting endpoint "
+echo "$sep Deleting endpoint "
 az ml online-endpoint delete --name $ENDPOINT_NAME -y
